5c7ad421ee17fa1fa50eccc98873583f1a5120c4
CaMeLCa5e <jm273606@gmail.com>
2014-10-11 11:03:47 -0400

    Fizz Buzz
    if/ elif statements

---
+++ FizzBuzzExcercise.py
@@ -0,0 +1,26 @@
+
+def checkio(number):
+    #Your code here
+    #It's main function. Don't remove this function
+    if (number) % 3.0 == 0. and (number) % 5.0 == 0.0:
+        return "Fizz Buzz"
+    elif number % 3.0 == 0.0:
+        return "Fizz"
+    elif number % 5.0 == 0.0:
+        return "Buzz"
+    else:
+        print (number)
+    #It's using for auto-testing and must return a result for check.
+    
+    #replace this for solution
+    return str(number)
+    
+Checkio - solve digits multiplication
+    
+      list(number):
+        print (list)
+    if number in list == 0 
+        return 1
+    for number in list 
+        number += number*
+    return 1
